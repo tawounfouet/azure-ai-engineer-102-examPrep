6248e25a1b69a4feacdd0d62665aa90850cc4248
Thomas AWOUNFOUET <78394330+tawounfouet@users.noreply.github.com>
2024-06-07 16:39:43 +0000

    knowledge mining

--- mslearn-knowledge-mining/Labfiles/01-azure-search/UploadDocs.cmd
+++ mslearn-knowledge-mining/Labfiles/01-azure-search/UploadDocs.cmd
@@ -6,7 +6,6 @@ set subscription_id=YOUR_SUBSCRIPTION_ID
 set azure_storage_account=YOUR_AZURE_STORAGE_ACCOUNT_NAME
 set azure_storage_key=YOUR_AZURE_STORAGE_KEY
 
-
 echo Creating container...
 call az storage container create --account-name !azure_storage_account! --subscription !subscription_id! --name margies --public-access blob --auth-mode key --account-key !azure_storage_key! --output none
 
