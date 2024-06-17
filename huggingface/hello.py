64ac0a5e6337fd40e5f480391896ba184351bf47
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2024-06-17 17:33:30 +0200

    getting started with huggingface

---
+++ huggingface/hello.py
@@ -0,0 +1,16 @@
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/openai-community/gpt2"
+headers = {"Authorization": "Bearer hf_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"}
+
+
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+	
+output = query({
+	"inputs": "Can you please let us know more details about your ",
+})
+
+print(output)
