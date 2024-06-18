c6eddf626f277909141f783324c6328a26ba00e6
Thomas AWOUNFOUET <78394330+tawounfouet@users.noreply.github.com>
2024-06-18 08:45:24 +0000

    huggingface tutorial & course

--- huggingface/hello.py
+++ huggingface/hello.py
@@ -1,5 +1,8 @@
 import requests
 
+
+# 
+
 API_URL = "https://api-inference.huggingface.co/models/openai-community/gpt2"
 headers = {"Authorization": "Bearer hf_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"}
 
